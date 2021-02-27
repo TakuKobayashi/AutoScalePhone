90bf066e6286d00d36cc963bea5e9ae286879d6d
DESKTOP-2VGTF7G\taku <keep_slimbody@yahoo.co.jp>
2021-02-28 02:26:24 +0900

    twilioを追加してどんなものが返ってくるかやってみた

--- twiio-serverless/handler.js
+++ twiio-serverless/handler.js
@@ -1,11 +1,18 @@
 const logo = require('asciiart-logo');
 
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
+
 exports.handler = function (context, event, callback) {
+  const twiml = new VoiceResponse();
+  twiml.say(
+    {
+      language: 'ja-JP',
+      voice: 'woman',
+    },
+    'testtest',
+  );
   callback(
     null,
-    logo({
-      name: 'Twilio Runtime',
-      version: '1.0.0',
-    }).render()
+    twiml.toString()
   );
 };
